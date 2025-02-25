adbb4b48d77b7aa4135656085cfeb01da4e68a76
markli2018 <44456096+markli2018@users.noreply.github.com>
2025-02-25 15:23:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小昂', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '定期分享资源，福利和最新的互联网动态', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.xiaoang.site/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小昂网站提供高清电影、软件、电子书、课程、BT种子和福利资源的分享与下载，专注于为用户提供实用的互联网资源和福利。', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '这里提供有价值的教程、工具和资源，助你提升数字生活！', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
